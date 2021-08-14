6b861279433861d03fd4d5a08a9d92c21e438816
engrogani75 <engrogani75@gmail.com>
2021-08-14 07:03:37 +0600

    My psd 2
    This is the 2 psd to html my practise

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+// ======================
+// Bx slider js
+// =======================
+
+
+$(document).ready(function(){
+    $('.slider_area').bxSlider();
+  });
+
+
+  $(document).ready(function () {
+    $('#main_menu').meanmenu({
+      meanMenuContainer: '#mobile_menu',
+      meanScreenWidth: "1024",
+    });
+  });
